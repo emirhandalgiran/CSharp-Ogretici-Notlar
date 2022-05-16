56cf6fee8ff017b5bcae46fb5739adaeecb63079
emirhandalgiran <78447253+emirhandalgiran@users.noreply.github.com>
2022-05-16 23:22:37 +0300

    Add files via upload

---
+++ 32-Hata Denetimi(try-catch-finally)/hata_denetimi/hata_denetimi/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace hata_denetimi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSay1 = new System.Windows.Forms.TextBox();
+            this.txtSay2 = new System.Windows.Forms.TextBox();
+            this.btnTopla = new System.Windows.Forms.Button();
+            this.lblGoster = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(26, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1.Sayı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(26, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2.Sayı:";
+            // 
+            // txtSay1
+            // 
+            this.txtSay1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSay1.Location = new System.Drawing.Point(83, 20);
+            this.txtSay1.Name = "txtSay1";
+            this.txtSay1.Size = new System.Drawing.Size(100, 23);
+            this.txtSay1.TabIndex = 2;
+            // 
+            // txtSay2
+            // 
+            this.txtSay2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSay2.Location = new System.Drawing.Point(83, 52);
+            this.txtSay2.Name = "txtSay2";
+            this.txtSay2.Size = new System.Drawing.Size(100, 23);
+            this.txtSay2.TabIndex = 3;
+            // 
+            // btnTopla
+            // 
+            this.btnTopla.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTopla.Location = new System.Drawing.Point(189, 20);
+            this.btnTopla.Name = "btnTopla";
+            this.btnTopla.Size = new System.Drawing.Size(75, 55);
+            this.btnTopla.TabIndex = 4;
+            this.btnTopla.Text = "Topla";
+            this.btnTopla.UseVisualStyleBackColor = true;
+            this.btnTopla.Click += new System.EventHandler(this.btnTopla_Click);
+            // 
+            // lblGoster
+            // 
+            this.lblGoster.AutoSize = true;
+            this.lblGoster.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblGoster.Location = new System.Drawing.Point(285, 23);
+            this.lblGoster.Name = "lblGoster";
+            this.lblGoster.Size = new System.Drawing.Size(42, 46);
+            this.lblGoster.TabIndex = 5;
+            this.lblGoster.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Tan;
+            this.ClientSize = new System.Drawing.Size(345, 94);
+            this.Controls.Add(this.lblGoster);
+            this.Controls.Add(this.btnTopla);
+            this.Controls.Add(this.txtSay2);
+            this.Controls.Add(this.txtSay1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Topla";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSay1;
+        private System.Windows.Forms.TextBox txtSay2;
+        private System.Windows.Forms.Button btnTopla;
+        private System.Windows.Forms.Label lblGoster;
+    }
+}
+
---
+++ 32-Hata Denetimi(try-catch-finally)/hata_denetimi/hata_denetimi/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hata_denetimi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTopla_Click(object sender, EventArgs e)
+        {
+            try//Çalışmasını istediğimiz kodlar burda
+            {
+                int say1, say2,sonuc;
+                say1 = Convert.ToInt32(txtSay1.Text);
+                say2 = Convert.ToInt32(txtSay2.Text);
+                sonuc = say1 + say2;
+                lblGoster.Text = sonuc.ToString();
+
+            }
+            catch//hata durumunda devreye girer
+            {
+                MessageBox.Show("Bir hatayla karşılaşıldı. Lütfen sadece sayı girin","Hata",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            finally// buradaki kodlar her koşulda çalışır
+            {
+                txtSay1.Clear();
+                txtSay2.Clear();
+            }
+        }
+    }
+}
