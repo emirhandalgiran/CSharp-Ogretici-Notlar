52760f404ece587bac0d219e9c37a3a1906c2110
Cebrail Emirhan Dalgıran <78447253+emirhandalgiran@users.noreply.github.com>
2024-11-27 12:02:03 +0300

    Add files via upload

---
+++ 31-Sayı Tahmin Oyunu/Sayi_Tahmin_Oyunu/Sayi_Tahmin_Oyunu/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Sayi_Tahmin_Oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSayi = new System.Windows.Forms.TextBox();
+            this.btnTahmin = new System.Windows.Forms.Button();
+            this.lblHak = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1-10 arasında bir sayı ürettim. \r\nBil bakalım...";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtSayi
+            // 
+            this.txtSayi.Location = new System.Drawing.Point(42, 90);
+            this.txtSayi.Name = "txtSayi";
+            this.txtSayi.Size = new System.Drawing.Size(214, 26);
+            this.txtSayi.TabIndex = 1;
+            // 
+            // btnTahmin
+            // 
+            this.btnTahmin.Location = new System.Drawing.Point(67, 122);
+            this.btnTahmin.Name = "btnTahmin";
+            this.btnTahmin.Size = new System.Drawing.Size(153, 41);
+            this.btnTahmin.TabIndex = 2;
+            this.btnTahmin.Text = "TAHMİN ET";
+            this.btnTahmin.UseVisualStyleBackColor = true;
+            this.btnTahmin.Click += new System.EventHandler(this.btnTahmin_Click);
+            // 
+            // lblHak
+            // 
+            this.lblHak.AutoSize = true;
+            this.lblHak.Location = new System.Drawing.Point(106, 67);
+            this.lblHak.Name = "lblHak";
+            this.lblHak.Size = new System.Drawing.Size(56, 20);
+            this.lblHak.TabIndex = 4;
+            this.lblHak.Text = "Hak=3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOrange;
+            this.ClientSize = new System.Drawing.Size(293, 204);
+            this.Controls.Add(this.lblHak);
+            this.Controls.Add(this.btnTahmin);
+            this.Controls.Add(this.txtSayi);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sayı Tahmin Oyunu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSayi;
+        private System.Windows.Forms.Button btnTahmin;
+        private System.Windows.Forms.Label lblHak;
+    }
+}
+
---
+++ 31-Sayı Tahmin Oyunu/Sayi_Tahmin_Oyunu/Sayi_Tahmin_Oyunu/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sayi_Tahmin_Oyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //randomm nesnesi oluşturulması gerekir
+        Random rastgele = new Random();
+        int sayi,tahmin,sayac=3;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            //1-10 arasında rastgele sayı üreteceğiz
+            sayi = rastgele.Next(1, 11);
+        }
+
+        private void btnTahmin_Click(object sender, EventArgs e)
+        {
+            tahmin=Convert.ToInt32(txtSayi.Text);
+            if (sayac==0)
+            {
+                MessageBox.Show("Hakkınız bitti\n sayı değeri="+sayi);
+                sayi = rastgele.Next(1, 11);
+                sayac = 3;
+                lblHak.Text = "Hak=" + sayac;
+            }
+            else
+            {
+                if (sayi == tahmin)
+                {
+                    MessageBox.Show("Tebrikler bildin");
+                    sayi = rastgele.Next(1, 11);
+                    sayac = 3;
+                    lblHak.Text = "Hak=" + sayac;
+                    
+                }
+                else
+                {
+                    sayac--;
+                    lblHak.Text = "Hak=" + sayac;
+                    MessageBox.Show("Üzgünüm bilemedin");
+                }
+            }
+            
+        }
+    }
+}
