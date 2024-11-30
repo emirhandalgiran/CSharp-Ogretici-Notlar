d31fb4b8b2cd3f16c99132e0d36eac479ffab37d
Cebrail Emirhan Dalgıran <78447253+emirhandalgiran@users.noreply.github.com>
2024-11-30 08:58:23 +0300

    Add files via upload

---
+++ 30-Do While örneği/do_while_ornegi/do_while_ornegi/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace do_while_ornegi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnYazdir_Click(object sender, EventArgs e)
+        {
+            int sayi = 0;
+            do
+            {
+                richTextBox1.Text = richTextBox1.Text + sayi;
+                sayi++;
+            } while (sayi<=30);
+        }
+    }
+}
