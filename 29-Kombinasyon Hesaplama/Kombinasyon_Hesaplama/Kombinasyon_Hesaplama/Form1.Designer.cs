0101be84a5ea3ce0bfa74afb16137be7fa0e47a0
Cebrail Emirhan Dalgıran <78447253+emirhandalgiran@users.noreply.github.com>
2024-12-08 22:35:53 +0300

    Add files via upload

---
+++ 29-Kombinasyon Hesaplama/Kombinasyon_Hesaplama/Kombinasyon_Hesaplama/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace Kombinasyon_Hesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.txtR = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(13, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "n:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(13, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "r:";
+            // 
+            // txtN
+            // 
+            this.txtN.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtN.Location = new System.Drawing.Point(37, 33);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(79, 23);
+            this.txtN.TabIndex = 0;
+            // 
+            // txtR
+            // 
+            this.txtR.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtR.Location = new System.Drawing.Point(37, 71);
+            this.txtR.Name = "txtR";
+            this.txtR.Size = new System.Drawing.Size(79, 23);
+            this.txtR.TabIndex = 1;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(129, 32);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(75, 62);
+            this.btnHesapla.TabIndex = 2;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(14, 124);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(0, 17);
+            this.lblSonuc.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(229, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Kombinasyon Hesaplama Programı";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnHesapla;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.ClientSize = new System.Drawing.Size(245, 184);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.txtR);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.TextBox txtR;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ 29-Kombinasyon Hesaplama/Kombinasyon_Hesaplama/Kombinasyon_Hesaplama/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kombinasyon_Hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            int r, n, faktoriyelN=1,faktoriyelR=1,faktoriyelNR=1,sonuc;
+            n = Convert.ToInt32(txtN.Text);
+            r = Convert.ToInt32(txtR.Text);
+
+            //n değeri r değerinden küçük olamayacağı için kontolünü sağlamamız gerekiyor
+            if (n<r)
+            {
+                MessageBox.Show("n değeri r değerinden küçük olamaz");
+            }
+
+            else
+            {
+                //n! değerini hesaplıyoruz.
+                for (int i = 1; i <= n; i++)
+                {
+                    faktoriyelN = faktoriyelN * i;
+                }
+
+                //(n-r)! değerini hesaplıyoruz
+                for (int i = 1; i <= (n - r); i++)
+                {
+                    faktoriyelNR = faktoriyelNR * i;
+                }
+
+                //r! değerini hesaplıyoruz
+                for (int i = 1; i <= r; i++)
+                {
+                    faktoriyelR = faktoriyelR * i;
+                }
+                sonuc = faktoriyelN / (faktoriyelNR * faktoriyelR);
+                lblSonuc.Text = n + "-" + r + " kombinasyon sonucu=" + sonuc;
+            }
+
+            //textboxları temizliyoruz
+            txtN.Clear();
+            txtR.Clear();
+        }
+    }
+}
